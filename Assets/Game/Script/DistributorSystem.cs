8b05b3181d917aba9233212bac36977ee2c42577
COURTOIS Rudy <rudy.courtois@hotmail.com>
2026-09-06 16:46:58 +0200

    Add SO and basic scripting
    - Basic main script for distributor;
    - Add SO for basic produit;

--- Assets/Game/Script/DistributorSystem.cs
+++ Assets/Game/Script/DistributorSystem.cs
@@ -5,4 +5,5 @@ public class DistributorSystem : MonoBehaviour
     private float price;
     private float currntMoney;
     
+
 }
---
+++ Assets/Game/ScriptableObject/Produit/SO_BaseProduit.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseProduit", menuName = "Scriptable Objects/SO_BaseProduit")]
+public class SO_BaseProduit : ScriptableObject
+{
+    
+}
