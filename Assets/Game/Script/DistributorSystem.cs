84a66f837d67c6a963f92cc4334b7119dc0c7ad8
COURTOIS Rudy <rudy.courtois@hotmail.com>
2026-09-06 17:33:57 +0200

    Parameters SO + Main Script
    - Add parameters for SO;
    - Add some script to the distributor script;
    - Add IOS Dev Support for mobile IOS;

--- Assets/Game/Script/DistributorSystem.cs
+++ Assets/Game/Script/DistributorSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DistributorSystem : MonoBehaviour
@@ -5,5 +6,27 @@ public class DistributorSystem : MonoBehaviour
     private float price;
     private float currntMoney;
     
+    [SerializeField] private SO_BaseProduit currentProduct;
 
+    [SerializeField] private SO_BaseProduit chocolate;
+    [SerializeField] private SO_BaseProduit chips;
+    [SerializeField] private List<SO_BaseProduit> listProduct = new List<SO_BaseProduit>();
+
+    private void SetCurrentProduct(SO_BaseProduit _newCurrentProduit)
+    {
+        if(_newCurrentProduit == null) Debug.LogError("there is no new current product");
+
+        currentProduct = null;
+        currentProduct = _newCurrentProduit;
+    }
+
+    public void GetChocolate()
+    {
+        SetCurrentProduct(chocolate);
+    }
+
+    public void GetChips()
+    {
+        SetCurrentProduct(chips);
+    }
 }
--- Assets/Game/ScriptableObject/Produit/SO_BaseProduit.cs
+++ Assets/Game/ScriptableObject/Produit/SO_BaseProduit.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BaseProduit", menuName = "Scriptable Objects/SO_BaseProduit")]
 public class SO_BaseProduit : ScriptableObject
 {
-    
+    public float price;
+    public string name;
+    public int id;
 }
